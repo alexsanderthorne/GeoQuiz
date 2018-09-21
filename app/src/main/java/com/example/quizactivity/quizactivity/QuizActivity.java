ee1439abc68fe64ada760db5db067468f48a8225
ALEXSANDER LUCIANO ARAUJO <20171D13GR0012@LABS.GARANHUNS.IFPE>
2018-09-20 21:53:52 -0300

    3.0

--- app/src/main/java/com/example/quizactivity/quizactivity/QuizActivity.java
+++ app/src/main/java/com/example/quizactivity/quizactivity/QuizActivity.java
@@ -2,22 +2,34 @@ package com.example.quizactivity.quizactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+
 public class QuizActivity extends AppCompatActivity {
 
-    private ImageButton mTrueButton;
-    private ImageButton mFalseButton;
+    private static final String TAG = "QuizActivity";
+
+    private Button mTrueButton;
+    private Button mFalseButton;
     private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mTextView;
     private int mCurrentIndex;
 
     Question[] mQuestions = new Question[]{
             new Question(R.string.pergunta1, false),
-            new Question(R.string.pergunta2, false),
+            new Question(R.string.pergunta2, true),
+            new Question(R.string.pergunta3, false),
+            new Question(R.string.pergunta4, false),
+            new Question(R.string.pergunta5, true),
     };
 
     private void updateQuestion() {
@@ -34,18 +46,65 @@ public class QuizActivity extends AppCompatActivity {
                 R.string.incorrect_toast;
         Toast.makeText(this, messageResId,
                 Toast.LENGTH_SHORT).show();
+
+        //if(messageResId == "Correto!")
+
     }
 
+
+    public QuizActivity() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"onStart" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume" , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart" , Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (ImageButton) findViewById(R.id.true_button);
-        mFalseButton = (ImageButton) findViewById(R.id.false_button);
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (ImageButton) findViewById(R.id.imageButton2);
+        mPrevButton = (ImageButton) findViewById(R.id.imageButton4);
         mTextView = (TextView) findViewById(R.id.textoid);
+
+        Collections.shuffle(Arrays.asList(mQuestions));
 
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +117,7 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            checkAnswer(false);
+                checkAnswer(false);
             }
         });
 
@@ -67,8 +126,26 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestions.length;
                 updateQuestion();
+                //é necessário um contador para mostrar a pontuação?
             }
         });
+
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestions.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestions.length;
+                updateQuestion();
+            }
+        });
+
 
 
     }
